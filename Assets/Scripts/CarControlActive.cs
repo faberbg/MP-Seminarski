782baa3e741b64f4c3c3c82353558dfb6bfd86fd
faberbg <58091336+faberbg@users.noreply.github.com>
2022-08-12 18:48:32 +0200

    AI krece u isto vreme

--- Assets/Scripts/CarControlActive.cs
+++ Assets/Scripts/CarControlActive.cs
@@ -6,10 +6,12 @@ using UnityStandardAssets.Vehicles.Car;
 public class CarControlActive : MonoBehaviour
 {
     public GameObject CarControl;
+    public GameObject CarControl2;
     // Start is called before the first frame update
     void Start()
     {
         CarControl.GetComponent<CarController>().enabled = true;
+        CarControl2.GetComponent<CarAIControl>().enabled = true;
     }
 
     
